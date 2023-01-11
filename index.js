7980142baf2f2b682da79bcd56713be139d2ef19
Mahmud <rainbowmahmud@gmail.com>
2023-01-11 20:11:45 +0600

    new attempts to fectch mongo connection problem

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 
+const {connectToDatabase} = require("./lib/mongodb")
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,51 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 
-/**
- * DB Connection
- * */
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hktestcluster.mtebihe.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+app.get("/", async (req, res) => {
+  const {database} = await connectToDatabase();
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     console.log("db is connected");
+  const collection = database.collection(process.env.HKTEST_ATLAS_COLLECTION);
 
-//     // Sectors
-//     // Collections
-//     const sectorCollection = client
-//       .db("hk-test-101")
-//       .collection("sectorCollection");
+  const sectors = await collection.find({}).toArray();
 
-
-//       // // base url
-// app.get("/", async (req, res) => {
-//   const sectors = await sectorCollection.find().toArray();
-
-//   res.send(sectors)
-// });
-
-    
-//   } finally {
-//     // await client.close();
-//   }
-// }
-
-// base url 
-app.get("/", (req, res) => {
-  res.send("Server is working.")
+  res.status(200).json(sectors)
 })
-
-// run().catch(console.dir);
-
-
-
-
 
 // listen to the port
 app.listen(port, () => {
