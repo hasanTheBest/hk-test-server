938c21a786f611c4eb34010d290e57b0dff86ec7
Mahmud <rainbowmahmud@gmail.com>
2023-01-12 00:20:53 +0600

    addUserCollection

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const {connectToDatabase} = require("./lib/mongodb")
+const { connectToDatabase } = require("./lib/mongodb")
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -12,15 +12,25 @@ app.use(cors());
 app.use(express.json());
 
 
+// GET SECTORS
 app.get("/", async (req, res) => {
-  const {database} = await connectToDatabase();
-
+  
+  const { database } = await connectToDatabase();
   const collection = database.collection(process.env.HKTEST_ATLAS_COLLECTION);
 
   const sectors = await collection.find({}).toArray();
 
   res.status(200).json(sectors)
 })
+
+// ADD A USER WITH SECTOR
+router.post("/add", async (req, res) => {
+  const { database } = await connectToDatabase();
+  const collection = database.collection("addUserCollection");
+  const result = await collection.insertOne(req.body);
+
+  res.send(result);
+});
 
 // listen to the port
 app.listen(port, () => {
